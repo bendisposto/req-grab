828733e729a496b281712aaf9c1e426fe82d3a3c
Jens Bendisposto <jens@bendisposto.de>
2015-06-21 20:37:09 +0200

    fix linebreaks

--- src/req_grab/grabber.clj
+++ src/req_grab/grabber.clj
@@ -8,15 +8,24 @@
            java.io.File)
   (:gen-class))
 
-(defn text-of-pdf
+(defn- text-of-pdf
   [filename]
   (with-open [pd (PDDocument/load (io/as-url (File. filename)))]
     (let [stripper (PDFTextStripper.)]
       (.getText stripper pd))))
 
-(def text (text-of-pdf "HD-CaseStudy-Description.pdf"))
-
 (def get-sentences (nlp/make-sentence-detector "models/en-sent.bin"))
 (def tokenize (nlp/make-tokenizer "models/en-token.bin"))
 (def chunker (treebank/make-treebank-chunker "models/en-chunker.bin"))
 (def pos-tag (nlp/make-pos-tagger "models/en-pos-maxent.bin"))
+
+(defn- fix-linebreaks [sentence]
+  (let [complex-breaks (.replaceAll sentence "-\n" "")]
+    (.replaceAll complex-breaks "\n" " ")))
+
+(defn read-sentences [filename]
+  (let [text (text-of-pdf filename)
+        sentences (map fix-linebreaks (get-sentences text))]
+    sentences))
+
+(def text (read-sentences "HD-CaseStudy-Description.pdf"))
